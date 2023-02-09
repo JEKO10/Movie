eed85132ac44fbfa2d29a3627228ecb16cef27a8
Aleksa Bubanja <aleksa3b@gmail.com>
2023-02-09 23:59:31 +0100

    pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,40 @@
+import { Switch, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Films from "./pages/Films";
+import SingleMovie from "./pages/SingleMovie";
+import Watchlist from "./pages/Watchlist";
+import Lists from "./pages/Lists";
+import Reviews from "./pages/Reviews";
+import Likes from "./pages/Likes";
+
 function App() {
-  return <div className="App">sad</div>;
+  return (
+    <Switch>
+      <Route path="/" exact>
+        <Home />
+      </Route>
+      <Route path="/profile">
+        <Profile />
+      </Route>
+      <Route path="reviews">
+        <Reviews />
+      </Route>
+      <Route path="/likes">
+        <Likes />
+      </Route>
+      <Route path="/watchlist">
+        <Watchlist />
+      </Route>
+      <Route path="/films">
+        <Films />
+      </Route>
+      <Route path="/lists">
+        <Lists />
+      </Route>
+      <Route children={<SingleMovie />} />
+    </Switch>
+  );
 }
 
 export default App;
