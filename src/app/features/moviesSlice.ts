17c418065ee397ffb5de92c00dd369be2cf89563
Aleksa Bubanja <aleksa3b@gmail.com>
2023-02-13 23:43:30 +0100

    getting movies data

--- src/app/features/moviesSlice.ts
+++ src/app/features/moviesSlice.ts
@@ -1,9 +1,46 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const url = `https://api.themoviedb.org/3/movie/297802?api_key=${process.env.REACT_APP_API_KEY}&append_to_response=images,credits`;
+
+type InitialStateType = {
+  isLoading: boolean;
+  allMovies: string[];
+};
+
+const initialState: InitialStateType = {
+  isLoading: true,
+  allMovies: [],
+};
+
+const getMovies = createAsyncThunk(
+  "movies/getMovies",
+  async (query: string, thunkAPI) => {
+    try {
+      const resp = await axios(url);
+      return resp.data;
+    } catch (error: any) {
+      return thunkAPI.rejectWithValue(error.response);
+    }
+  }
+);
 
 const moviesSlice = createSlice({
   name: "movies",
-  initialState: {
-    isLoading: true,
-    movies: [],
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [getMovies.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [getMovies.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.allMovies = action.payload;
+    },
+    [getMovies.rejected]: (state, action) => {
+      state.isLoading = false;
+    },
   },
 });
+
+export const { reducer } = moviesSlice;
--- src/app/store.tsx
+++ src/app/store.tsx
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { reducer as moviesReducer } from "./features/moviesSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    movies: moviesReducer,
+  },
 });
