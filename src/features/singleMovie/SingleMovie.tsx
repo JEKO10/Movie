ea97ecbee385fa4795e5746c802c3ead1fc192ed
Aleksa Bubanja <aleksa3b@gmail.com>
2023-02-21 23:58:58 +0100

    displaying info about movie

--- src/features/singleMovie/SingleMovie.tsx
+++ src/features/singleMovie/SingleMovie.tsx
@@ -4,16 +4,50 @@ import { useAppDispatch, useAppSelector } from "../../common/hooks";
 import { getMovie } from "./singleMovieSlice";
 
 const SingleMovie = () => {
-  const { movieInfo } = useAppSelector((store) => store.singleMovie);
+  const {
+    title,
+    backdrop_path,
+    belongs_to_collection,
+    genres,
+    budget,
+    homepage,
+    imdb_id,
+    overview,
+    popularity,
+    poster_path,
+    production_companies,
+    production_countries,
+    release_date,
+    revenue,
+    runtime,
+    spoken_languages,
+    status,
+    vote_average,
+    vote_count,
+  } = useAppSelector((store) => store.singleMovie.movieInfo);
   const { id } = useParams();
   const dispatch = useAppDispatch();
+  const posterUrl = "https://image.tmdb.org/t/p/w300/";
 
   useEffect(() => {
     dispatch(getMovie(id));
-    console.log(movieInfo);
   }, []);
 
-  return <section>SingleMovie</section>;
+  return (
+    <section className="singleMovie">
+      <img src={posterUrl + backdrop_path} alt="BANNER" />
+      <article className="wrapper">
+        <img src={posterUrl + poster_path} alt="POSTER" />
+        <div>
+          <h2>{title}</h2>
+          <p>{overview}</p>
+          <h3>{runtime}min</h3>
+          <h4>{release_date}</h4>
+        </div>
+        <article className="collection"></article>
+      </article>
+    </section>
+  );
 };
 
 export default SingleMovie;
