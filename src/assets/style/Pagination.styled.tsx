593989ec5d59a0fd28f6214f1a57c29cd82321f0
Aleksa Bubanja <aleksa3b@gmail.com>
2023-05-26 23:44:03 +0200

    better pagination

--- src/assets/style/Pagination.styled.tsx
+++ src/assets/style/Pagination.styled.tsx
@@ -1,24 +1,28 @@
 import styled from "styled-components";
 
-import { flexMixin, primaryColor, secondaryColor } from "./GlobalStyles";
+import { flexMixin, primaryColor } from "./GlobalStyles";
 
 export const PaginationList = styled.ul`
   ${flexMixin({ justify: "center", align: "center" })}
   list-style-type: none;
-  margin-bottom: 4rem;
+  margin: 2rem 0;
   flex-wrap: wrap;
 
   li {
+    background-color: ${primaryColor};
     margin: 10px 5px;
     padding: 10px 15px;
     border: 1px solid #000;
     border-radius: 5px;
     cursor: pointer;
-    background-color: ${primaryColor};
+
+    svg {
+      transform: translateY(11%);
+    }
 
     &:hover {
-      background-color: ${secondaryColor};
-      color: #fff;
+      background-color: #fff !important;
+      color: ${primaryColor};
     }
   }
 `;
--- src/features/discoverMovies/components/Pagination.tsx
+++ src/features/discoverMovies/components/Pagination.tsx
@@ -1,7 +1,12 @@
-import React, { useEffect } from "react";
+import React, { useState } from "react";
+import {
+  MdKeyboardDoubleArrowLeft,
+  MdKeyboardDoubleArrowRight,
+} from "react-icons/md";
 
 import { PaginationList } from "../../../assets/style/Pagination.styled";
 import { useAppDispatch, useAppSelector } from "../../../common/hooks";
+import { Loader, LoaderWrapper } from "../../../common/Loader";
 import { getDiscoverMovies } from "../discoverMoviesSlice";
 
 type PaginationProps = {
@@ -9,30 +14,67 @@ type PaginationProps = {
 };
 
 const Pagination: React.FC<PaginationProps> = ({ id }) => {
-  const { totalPages = 1 } = useAppSelector((store) => store.discoverMovies);
+  const [currentPage, setCurrentPage] = useState(1);
+  const { totalPages = 1, isLoading } = useAppSelector(
+    (store) => store.discoverMovies
+  );
   const pages = [];
   const dispatch = useAppDispatch();
 
-  for (let i = 1; i <= totalPages; i++) {
+  for (let i = 0; i <= totalPages; i++) {
     pages.push(i);
   }
 
-  useEffect(() => {
-    console.log(totalPages);
-  }, []);
+  const handlePageClick = (page: number) => {
+    window.scrollTo(0, 0);
+    dispatch(getDiscoverMovies({ id, page }));
+    setCurrentPage(page);
+  };
 
+  if (isLoading) {
+    <LoaderWrapper>
+      <Loader />
+    </LoaderWrapper>;
+  }
   return (
     <PaginationList>
-      {pages.map((page: number) => {
-        return (
-          <li
-            key={page}
-            onClick={() => dispatch(getDiscoverMovies({ id, page }))}
-          >
-            {page}
-          </li>
-        );
-      })}
+      <li
+        onClick={() => {
+          dispatch(getDiscoverMovies({ id, page: 1 }));
+          setCurrentPage(1);
+        }}
+      >
+        <MdKeyboardDoubleArrowLeft />
+      </li>
+      {pages
+        .slice(
+          ...(currentPage === 2
+            ? [currentPage - 1, currentPage + 4]
+            : currentPage === 1
+            ? [currentPage, currentPage + 5]
+            : [currentPage - 2, currentPage + 3])
+        )
+        .map((page: number) => {
+          return (
+            <li
+              style={{
+                background: currentPage === page ? "#dda824" : "",
+              }}
+              key={page}
+              onClick={() => handlePageClick(page)}
+            >
+              {page}
+            </li>
+          );
+        })}
+      <li
+        onClick={() => {
+          dispatch(getDiscoverMovies({ id, page: pages.length - 1 }));
+          setCurrentPage(pages.length - 1);
+        }}
+      >
+        <MdKeyboardDoubleArrowRight />
+      </li>
     </PaginationList>
   );
 };
