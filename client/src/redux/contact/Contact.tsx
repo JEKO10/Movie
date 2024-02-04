1e1560b915f4f721d38325add4a38a23b78f1f8e
Aleksa <aleksa3b@gmail.com>
2024-02-04 23:39:33 +0100

    contact form states

--- client/src/redux/contact/Contact.tsx
+++ client/src/redux/contact/Contact.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 import contactImg from "../../assets/images/contact.jpg";
@@ -12,6 +12,9 @@ import { setQuery } from "../navbar/navbarSlice";
 
 const Contact = () => {
   const dispatch = useAppDispatch();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
 
   useEffect(() => {
     dispatch(setQuery(""));
@@ -29,15 +32,28 @@ const Contact = () => {
         <ContactForm>
           <label>
             Your name
-            <input type="text" placeholder="Enter your name" />
+            <input
+              type="text"
+              placeholder="Enter your name"
+              onChange={(event) => setName(event.target.value)}
+            />
           </label>
           <label>
             Your email address
-            <input type="email" placeholder="Enter your email address" />
+            <input
+              type="email"
+              placeholder="Enter your email address"
+              onChange={(event) => setEmail(event.target.value)}
+            />
           </label>
           <label>
             Your message
-            <textarea cols={30} rows={5} placeholder="Your message"></textarea>
+            <textarea
+              cols={30}
+              rows={5}
+              placeholder="Your message"
+              onChange={(event) => setMessage(event.target.value)}
+            />
           </label>
           <button type="submit">Send</button>
         </ContactForm>
