15b1f3aed1632fc1b49cbea3ed523a71d14647db
Aleksa <aleksa3b@gmail.com>
2024-04-15 22:38:20 +0200

    useOutsideClick for favorite modal

--- client/src/redux/profile/settings/FavoriteModal.tsx
+++ client/src/redux/profile/settings/FavoriteModal.tsx
@@ -1,19 +1,28 @@
 import React from "react";
 import { IoMdClose } from "react-icons/io";
 
-import { useAppDispatch, useAppSelector } from "../../../common/hooks";
+import {
+  useAppDispatch,
+  useAppSelector,
+  useOutsideClick
+} from "../../../common/hooks";
 import { handleExit, ModalProps } from "../../../common/modals/modalUtils";
 import { FixedContainer, Modal as Container } from "../../logModal/Log.style";
 import Input from "../../navbar/components/Input";
+import { setIsLogOpen } from "../../navbar/navbarSlice";
 import { setIsFavoriteOpen } from "../profileSlice";
 
 const FavoriteModal: React.FC<ModalProps> = ({ isClosing, setIsClosing }) => {
   const { inputValue } = useAppSelector((store) => store.navbar);
   const dispatch = useAppDispatch();
 
+  const { ref } = useOutsideClick(setIsLogOpen, () =>
+    handleExit(setIsClosing, dispatch, setIsFavoriteOpen)
+  );
+
   return (
     <FixedContainer>
-      <Container isClosing={isClosing}>
+      <Container ref={ref} isClosing={isClosing}>
         <h3>Pick a favorite film</h3>
         <IoMdClose
           onClick={() => handleExit(setIsClosing, dispatch, setIsFavoriteOpen)}
