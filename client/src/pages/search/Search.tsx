05deb0a4ed47b505248a1573a3235c4ea40f81c0
Aleksa <aleksa3b@gmail.com>
2024-04-24 23:40:03 +0200

    better pagination for search page

--- client/src/pages/search/Search.tsx
+++ client/src/pages/search/Search.tsx
@@ -1,8 +1,13 @@
 import { isRejectedWithValue } from "@reduxjs/toolkit";
 import axios, { isAxiosError } from "axios";
 import React, { useEffect, useState } from "react";
+import {
+  MdKeyboardDoubleArrowLeft,
+  MdKeyboardDoubleArrowRight
+} from "react-icons/md";
 import { Link, useParams } from "react-router-dom";
 
+import { PaginationList } from "../../assets/style/Pagination.styled";
 import { Loader, LoaderWrapper } from "../../common/Loader";
 import { SearchData } from "../../common/types/typesTS";
 import { Underline } from "../../redux/singleMovie/SingleMovie.styled";
@@ -14,6 +19,13 @@ const Search = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const { inputValue } = useParams();
   const posterUrl = "https://image.tmdb.org/t/p/w342/";
+  const [totalResults, setTotalResults] = useState(1);
+  const pages = [];
+  const totalPages = Math.ceil(totalResults / 20);
+
+  for (let i = 0; i <= totalPages; i++) {
+    pages.push(i);
+  }
 
   const getSearchData = async () => {
     setIsLoading(true);
@@ -27,25 +39,27 @@ const Search = () => {
         }&query=${formattedInputValue}&sort_by=popularity.desc&page=${currentPage}`
       );
 
-      // const creditPromises = response.data.results.map(
-      //   async (movie: SearchData) => {
-      //     if (movie.media_type === "movie") {
-      //       const creditsResponse = await axios.get(
-      //         `https://api.themoviedb.org/3/movie/${movie.id}/credits?api_key=${import.meta.env.VITE_API_KEY}`
-      //       );
+      setTotalResults(response.data.total_results);
 
-      //       movie.credits = creditsResponse.data;
-      //     } else if (movie.media_type === "tv") {
-      //       const creditsResponse = await axios.get(
-      //         `https://api.themoviedb.org/3/tv/${movie.id}/credits?api_key=${import.meta.env.VITE_API_KEY}`
-      //       );
+      const creditPromises = response.data.results.map(
+        async (movie: SearchData) => {
+          if (movie.media_type === "movie") {
+            const creditsResponse = await axios.get(
+              `https://api.themoviedb.org/3/movie/${movie.id}/credits?api_key=${import.meta.env.VITE_API_KEY}`
+            );
 
-      //       movie.credits = creditsResponse.data;
-      //     }
-      //   }
-      // );
+            movie.credits = creditsResponse.data;
+          } else if (movie.media_type === "tv") {
+            const creditsResponse = await axios.get(
+              `https://api.themoviedb.org/3/tv/${movie.id}/credits?api_key=${import.meta.env.VITE_API_KEY}`
+            );
 
-      // await Promise.all(creditPromises);
+            movie.credits = creditsResponse.data;
+          }
+        }
+      );
+
+      await Promise.all(creditPromises);
 
       setSearchData(response.data.results);
       setIsLoading(false);
@@ -56,8 +70,15 @@ const Search = () => {
     }
   };
 
+  const handlePageClick = (page: number) => {
+    window.scrollTo(0, 0);
+    setCurrentPage(page);
+  };
+
   useEffect(() => {
     getSearchData();
+
+    console.log(totalResults);
   }, [inputValue, currentPage]);
 
   if (isLoading) {
@@ -93,7 +114,7 @@ const Search = () => {
                   <span>{movie.release_date?.slice(0, 4)}</span>
                 </h3>
                 <p>{movie.overview}</p>
-                {/* {movie.credits.crew.length !== 0 && (
+                {movie.credits.crew.length !== 0 && (
                   <h4>
                     Directed by{"  "}
                     <span>
@@ -106,24 +127,53 @@ const Search = () => {
                       }
                     </span>
                   </h4>
-                )} */}
+                )}
               </div>
             </Link>
             <Underline width={"100%"} margin="0 0 1rem" />
           </>
         ))}
       </article>
-      <div>
-        <button
-          onClick={() => setCurrentPage(currentPage - 1)}
-          disabled={currentPage === 1}
+      <PaginationList>
+        <li
+          onClick={() => {
+            handlePageClick(1);
+          }}
         >
-          Previous Page
-        </button>
-        <button onClick={() => setCurrentPage(currentPage + 1)}>
-          Next Page
-        </button>
-      </div>
+          <MdKeyboardDoubleArrowLeft />
+        </li>
+        {pages
+          .slice(
+            ...(currentPage === 2
+              ? [currentPage - 1, currentPage + 4]
+              : currentPage === 1
+                ? [currentPage, currentPage + 5]
+                : [currentPage - 2, currentPage + 3])
+          )
+          .map((page: number) => {
+            return (
+              <li
+                style={{
+                  background: currentPage === page ? "#dda824" : ""
+                }}
+                key={page}
+                onClick={() => {
+                  handlePageClick(page * 5);
+                  setCurrentPage(page);
+                }}
+              >
+                {page}
+              </li>
+            );
+          })}
+        <li
+          onClick={() => {
+            handlePageClick(pages.length - 1);
+          }}
+        >
+          <MdKeyboardDoubleArrowRight />
+        </li>
+      </PaginationList>
     </SearchContainer>
   );
 };
