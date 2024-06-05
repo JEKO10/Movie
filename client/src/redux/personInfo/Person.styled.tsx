1558387f21378438a7cf878925fd485642b6cd6e
Aleksa Bubanja <aleksa3b@gmail.com>
2024-06-05 21:32:44 +0000

    PersonInfo page responsive

--- client/src/redux/personInfo/Person.styled.tsx
+++ client/src/redux/personInfo/Person.styled.tsx
@@ -1,18 +1,34 @@
 import styled from "styled-components";
 
-import { flexMixin, paragraphColor } from "../../assets/style/GlobalStyles";
+import { devices, flexMixin, paragraphColor } from "../../assets/style/GlobalStyles";
 
 export const Person = styled.section`
   ${flexMixin({ justify: "space-between", align: "flex-start" })};
   color: ${paragraphColor};
 
   max-width: 80vw;
-
   margin: 3rem auto;
+
+  @media ${devices.laptopL} {
+    max-width: 90vw;
+  }
 
   article:first-of-type {
     grid-template-columns: repeat(5, 1fr);
-    margin: 0 5rem;
+    margin-right: 5rem;
+
+    @media ${devices.laptopS} {
+      grid-template-columns: repeat(4, 1fr);
+      margin-right: 2rem;
+    }
+
+    @media ${devices.tablet} {
+      grid-template-columns: repeat(3, 1fr);
+    }
+
+    @media ${devices.mobile} {
+      grid-template-columns: repeat(1, 1fr);
+    }
 
     img {
       border: none;
@@ -26,10 +42,21 @@ export const Person = styled.section`
   h2 {
     margin: 0.5rem 0;
     color: #fff;
+
+    @media ${devices.tablet} {
+      line-height: 25px;
+    }
   }
 
   h4 {
     margin: 0.2rem 0;
+
+    @media ${devices.tablet} {
+      font-weight: 100;
+      line-height: 20px;
+
+      margin: 0.5rem 0;
+    }
   }
 
   p {
@@ -45,6 +72,14 @@ export const PersonImg = styled.img`
   border-radius: 5px;
 
   transition: all 200ms ease;
+
+  @media ${devices.laptopS} {
+    width: 170px;
+  }
+
+  @media ${devices.tablet} {
+    width: 120px;
+  }
 
   &:hover {
     border: 2px solid #fff;
